2658016f1bc958761d6e6f83bb959666a7ab252c
nabdirasulov <nurbaabdirasul@gmail.com>
2025-05-28 17:58:17 -0500

    finish fixing sign in

---
+++ src/Security.java
@@ -0,0 +1,22 @@
+public class Security {
+    public static void main(String[] args) {
+        System.out.println("this is new policy of security");
+        System.out.println("this is new policy of security");
+        System.out.println("this is new policy of security");
+        System.out.println("this is new policy of security");
+        System.out.println("this is new policy of security");
+        System.out.println("this is new policy of security");
+        System.out.println("this is new policy of security");
+        System.out.println("this is new policy of security");
+        System.out.println("this is new policy of security");
+        System.out.println("this is new policy of security");
+        System.out.println("this is new policy of security");
+        System.out.println("this is new policy of security");
+        System.out.println("this is new policy of security");
+        System.out.println("this is new policy of security");
+        System.out.println("this is new policy of security");
+        System.out.println("this is new policy of security");
+        System.out.println("this is new policy of security");
+        System.out.println("this is new policy of security");
+    }
+}
